c87acf2ca52cb6816d28bb65d0bb7fe4cb89b83c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-21 13:49:44 -0700

    Add logo to interactive mode

--- jai/console.py
+++ jai/console.py
@@ -1,13 +1,22 @@
 import jai
 from jai.logger import log, Severity
+from termcolor import colored
+
+EXIT_MESSAGE = "Bye!"
 
 
 def input_wrapper():
-    inputted = input("jai> ")
-    print("")
+    try:
+        inputted = input("jai> ")
+        print("")
 
-    if inputted.upper() == "Q" or inputted.upper() == "QUIT":
-        print("Bye!")
+        if inputted.upper() == "Q" or inputted.upper() == "QUIT":
+            print(EXIT_MESSAGE)
+            exit(0)
+
+    # This will gracefully leave if ctrl+c is done
+    except KeyboardInterrupt:
+        print(f"\n{EXIT_MESSAGE}")
         exit(0)
 
     return inputted
@@ -15,15 +24,25 @@ def input_wrapper():
 
 def run_interactive():
 
-    logo = f"""
-       ___       _
-      |_  |     (_)     |   Documentation: https://github.com/JakeRoggenbuck/jai/wiki
-        | | __ _ _      |
-        | |/ _` | |     |   Version: {jai.__version__}
-    /\\__/ / (_| | |     |
-    \\____/ \\__,_|_|     |
-    """
+    line_1 = colored("   ___       _ ", "blue")
+    line_2 = colored("  |_  |     (_)", "blue")
+    line_3 = colored("    | | __ _ _ ", "blue")
+    line_4 = colored("    | |/ _` | |", "blue")
+    line_5 = colored("/\\__/ / (_| | |", "blue")
+    line_6 = colored("\\____/ \\__,_|_|", "blue")
 
+    lines = [line_1, line_2, line_3, line_4, line_5, line_6]
+    lines = [line + "    |    " for line in lines[1:]]
+
+    line_2, line_3, line_4, line_5, line_6 = lines
+
+    line_2 += "Documentation: https://github.com/JakeRoggenbuck/jai/wiki"
+    line_4 += f"Version: {colored(jai.__version__, 'green')}"
+
+    lines = [line_1, line_2, line_3, line_4, line_5, line_6]
+    lines = [line + "\n" for line in lines]
+
+    logo = "".join(lines)
     print(logo)
 
     while 1:
