30c5a1218d96daa674d849c06c7fbd6ebedf3b47
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-22 13:24:47 -0700

    Add new rules, move rules to ast

--- jai/ast.py
+++ jai/ast.py
@@ -1,4 +1,65 @@
-class ASTStmt:
-    def __init__(self, leftside, rightside):
-        self.leftside = leftside
-        self.rightside = rightside
+from typing import List
+from jai import Tokens
+
+
+class EmptyAssignment:
+    def __init__(
+        self,
+        type_name=0,
+        identifier=0,
+        semicolon=0,
+    ):
+        self.type_name = type_name
+        self.identifier = identifier
+        self.semicolon = semicolon
+
+        self.name: str = "empty assignment"
+        self.rule: List[Tokens] = [Tokens.TypeName, Tokens.Identifier, Tokens.Semicolon]
+
+        self.token = None
+
+    def filled(self):
+        return not self.type_name and not self.identifier and not self.semicolon
+
+    def __repr__(self):
+        return "EmptyAssignment(" + " ".join([str(a) for a in self.rule]) + ")"
+
+
+class IntAssignment:
+    def __init__(
+        self,
+        type_name=0,
+        identifier=0,
+        assignment=0,
+        numeric_literal=0,
+        semicolon=0,
+    ):
+        self.type_name = type_name
+        self.identifier = identifier
+        self.assignment = assignment
+        self.numeric_literal = numeric_literal
+        self.semicolon = semicolon
+
+        self.name: str = "int assignment"
+        self.rule: List[Tokens] = [
+            Tokens.TypeName,
+            Tokens.Identifier,
+            Tokens.Assignment,
+            Tokens.NumericLiteral,
+            Tokens.Semicolon,
+        ]
+
+        self.token = None
+
+    def filled(self):
+        return (
+            self.type_name is not NotImplemented
+            and self.identifier is not NotImplemented
+            and self.semicolon is not NotImplemented
+        )
+
+    def __repr__(self):
+        return "IntAssignment(" + " ".join([str(a) for a in self.rule]) + ")"
+
+
+RULES = [EmptyAssignment, IntAssignment]
--- jai/parser.py
+++ jai/parser.py
@@ -1,10 +1,9 @@
-from enum import Enum
 from jai import Lexer, Token, Settings, EMPTY_TOKEN, Tokens
 from jai.logger import Severity, log
 from typing import List
 from os.path import exists
 from jai.mode import Mode
-from jai.ast import ASTStmt
+from jai.ast import RULES
 from more_itertools import locate
 
 
@@ -69,69 +68,6 @@ def interpret_function_doc(doc: str) -> FunctionDoc:
                     type_stack.append(token)
 
     return FunctionDoc(doc, doctype, type_stack)
-
-
-class EmptyAssignment:
-    def __init__(
-        self,
-        type_name=0,
-        identifier=0,
-        semicolon=0,
-    ):
-        self.type_name = type_name
-        self.identifier = identifier
-        self.semicolon = semicolon
-
-        self.name: str = "empty assignment"
-        self.rule: List[Tokens] = [Tokens.TypeName, Tokens.Identifier, Tokens.Semicolon]
-
-        self.token = None
-
-    def filled(self):
-        return not self.type_name and not self.identifier and not self.semicolon
-
-    def __repr__(self):
-        return "EmptyAssignment(" + " ".join([str(a) for a in self.rule]) + ")"
-
-
-class IntAssignment:
-    def __init__(
-        self,
-        type_name=0,
-        identifier=0,
-        assignment=0,
-        numeric_literal=0,
-        semicolon=0,
-    ):
-        self.type_name = type_name
-        self.identifier = identifier
-        self.assignment = assignment
-        self.numeric_literal = numeric_literal
-        self.semicolon = semicolon
-
-        self.name: str = "int assignment"
-        self.rule: List[Tokens] = [
-            Tokens.TypeName,
-            Tokens.Identifier,
-            Tokens.Assignment,
-            Tokens.NumericLiteral,
-            Tokens.Semicolon,
-        ]
-
-        self.token = None
-
-    def filled(self):
-        return (
-            self.type_name is not NotImplemented
-            and self.identifier is not NotImplemented
-            and self.semicolon is not NotImplemented
-        )
-
-    def __repr__(self):
-        return "IntAssignment(" + " ".join([str(a) for a in self.rule]) + ")"
-
-
-RULES = [EmptyAssignment, IntAssignment]
 
 
 class Parser:
